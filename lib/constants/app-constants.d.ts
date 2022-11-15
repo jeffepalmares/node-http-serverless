3f7f870e2620503bff22ed10ef81c9745eeedd56
Jefferson Cardoso <jeffersoncardoso.s@gmail.com>
2022-11-15 16:22:35 -0300

    constants

--- lib/constants/app-constants.d.ts
+++ lib/constants/app-constants.d.ts
@@ -1,6 +1,6 @@
 export declare abstract class AppConstants {
     static readonly PARAMETER_REGEX_HOLDER = "PARAM_REGEX";
-    static readonly PARAMETER_REGEX = "([\\d,\\w,@,\\.,_,\\-,#]*)";
+    static readonly PARAMETER_REGEX = "([\\d,\\w,@,\\.,_,\\-,#,+]*)";
     static readonly QUERY_PARAMETER_REGEX = "(\\?.*)";
     static readonly DEFAULT_API_PARAMETER_REGEX: RegExp;
     static readonly CURLY_BRACKETS: RegExp;
